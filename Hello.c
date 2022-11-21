09e7367ec030b8d993dade8b79d45bf74e148793
29ardayildiz <ardayildiz029@gmail.com>
2022-11-21 19:57:40 +0300

    Added Fibonacci Sequence

--- Hello.c
+++ Hello.c
@@ -2,10 +2,27 @@
 
 int main(){
 
-    char s;
+//    char s;
 
     printf("Hello World!");
-    scanf("%c", &s);
+//    scanf("%c", &s);
+
+    int ilk_sayi = 1;
+	int ikinci_sayi = 1;
+	int i,n;
+		
+	printf("Fibonacci dizisinin kac terimini gormek isteriniz?: ");
+	scanf("%d",&n);
+	printf("%d\n%d\n", ilk_sayi, ikinci_sayi);
+	
+	for(i=0;i<n;i++){
+		
+		int top = ikinci_sayi;
+		ikinci_sayi += ilk_sayi;
+		ilk_sayi = top;
+	
+		printf("%d\n", ikinci_sayi);		
+	}
 
     return 0;
 }
